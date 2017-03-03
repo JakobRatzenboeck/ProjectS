be3c91287a6fbda1e4aff384a29a19c50b65bcb2
Johannes <Johannes@10.91.60.106>
2017-03-03 09:36:31 +0100

    jhadfsjkldfjklasdföjl

--- src/Autogenerate.java
+++ src/Autogenerate.java
@@ -32,6 +32,7 @@ public class Autogenerate {
 		return true;
 	}
 	//HIER WIRD DAS DREIER FELD GETESTET
+	//dföjklaslfjkasdölfjasdöfdjkl
 	public boolean dreierfeld(int x, int y, int nummber) {
 		if (y < 6) {
 			if (y < 3) {
