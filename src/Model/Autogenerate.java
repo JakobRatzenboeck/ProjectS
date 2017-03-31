229a76e2f99d85399f5193362eadb88a62290516
Jakob <Jakob@10.91.60.77>
2017-03-31 10:11:03 +0200

    Backtracking fertig!!!!!!!!!!!

--- src/Model/Autogenerate.java
+++ src/Model/Autogenerate.java
@@ -1,21 +1,16 @@
 package Model;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class Autogenerate {
+	private int fertigesfeld[][] = new int[9][9];
 	private int feld[][] = new int[9][9];
-	private int falsch[][] = new int[9][3];
 	private static int Staticid;
 	private int id;
 	private int ist;
 	private int soll;
-	private int imwegx;
-	private int imwegy;
-	private int x;
-	private int y;
+	private int x = 0;
+	private int y = 0;
 
 	public Autogenerate(int wieViele) {
 		setId(Staticid);
@@ -26,7 +21,6 @@ public class Autogenerate {
 	public Autogenerate() {
 		setId(Staticid);
 		Staticid++;
-		reihenfuellen();
 	}
 
 	/**
@@ -47,8 +41,6 @@ public class Autogenerate {
 	public boolean reihe(int x, int y, int number) {
 		for (int i = 0; i < 9; i++) {
 			if (feld[i][y] == number) {
-				imwegx = i;
-				imwegy = y;
 				return false;
 			}
 		}
@@ -58,8 +50,6 @@ public class Autogenerate {
 	public boolean spalte(int x, int y, int number) {
 		for (int i = 0; i < 9; i++) {
 			if (feld[x][i] == number) {
-				imwegx = x;
-				imwegy = i;
 				return false;
 			}
 		}
@@ -69,7 +59,77 @@ public class Autogenerate {
 	public boolean notfinished() {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				if (i == 0) {
+				if (feld[j][i] == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean backtracking() {
+		Random random = new Random();
+		int oldx = x;
+		int oldy = y;
+		if (notfinished()) {
+			int wert = random.nextInt(8) + 1;
+
+			for (int versuch = 0; versuch < 9; ++versuch) {
+				if (dreierfeld(x, y, wert) && reihe(x, y, wert) == true && spalte(x, y, wert)) {
+					feld[x][y] = wert;
+					if (x + 1 >= 9) {
+						x = 0;
+						y++;
+					} else {
+						x++;
+					}
+					if (backtracking()) {
+						return true;
+					}
+					// wenn nicht geklappt rueckgaengig
+					x = oldx;
+					y = oldy;
+					feld[x][y] = 0;
+				}
+				// neuer wert
+				if (wert + 1 > 9) {
+					wert = 1;
+				} else {
+					wert++;
+				}
+
+			}
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean dreierfeld(int x, int y, int number) {
+		int newx = 0;
+		int newy = 0;
+		if (x >= 0 && x <= 2) {
+			newx = 0;
+		}
+		if (x >= 3 && x <= 5) {
+			newx = 3;
+		}
+		if (x >= 6 && x <= 8) {
+			newx = 6;
+		}
+
+		if (y >= 0 && y <= 2) {
+			newy = 0;
+		}
+		if (y >= 3 && y <= 5) {
+			newy = 3;
+		}
+		if (y >= 6 && y <= 8) {
+			newy = 6;
+		}
+		for (int i = newy; i < newy + 3; i++) {
+			for (int j = newx; j < newx + 3; j++) {
+				if (feld[j][i] == number) {
 					return false;
 				}
 			}
@@ -77,128 +137,7 @@ public class Autogenerate {
 		return true;
 	}
 
-	public boolean backtracking() {
-		Random random = new Random();
-		int wert = 0;
-		if (notfinished()) {
-			wert = random.nextInt(8) + 1;
-		}
-		// if (grid is empty) {
-		// assign the empty grid with values (i)
-		// if (no numbers exists in same rows & same columns same as (i) & 3x3
-		// square (i) is currently in)
-		// fill in the number
-		// if (numbers exists in same rows | same columns same as (i) | 3x3
-		// square (i) is currently in)
-		// discard (i) and repick other values (i++)
-		// }
-		// else {
-		// while (nx < 9) {
-		// Proceed to next row grid(nx++, ny)
-		// if (nx equals 9) {
-		// reset nx = 1
-		// proceed to next column grid(nx,ny++)
-		// if (ny equals 9) {
-		// print solution
-		// }
-		// }
-		// }
-		// }
-		return true;
-	}
-
-	public boolean dreierfeld(int x, int y, int number) {
-		if (y < 6) {
-			if (y < 3) {
-				if (x < 6) {
-					if (x < 3) {
-						for (int i = 0; i < 3; i++) {
-							for (int j = 0; j < 3; j++) {
-								if (feld[j][i] == number) {
-									return false;
-								}
-							}
-						}
-					} else {
-						for (int i = 0; i < 3; i++) {
-							for (int j = 3; j < 6; j++) {
-								if (feld[j][i] == number) {
-									return false;
-								}
-							}
-						}
-					}
-				} else {
-					for (int i = 0; i < 3; i++) {
-						for (int j = 6; j < 9; j++) {
-							if (feld[j][i] == number) {
-								return false;
-							}
-						}
-					}
-				}
-			} else {
-				if (x < 6) {
-					if (x < 3) {
-						for (int i = 3; i < 6; i++) {
-							for (int j = 0; j < 3; j++) {
-								if (feld[j][i] == number) {
-									return false;
-								}
-							}
-						}
-					} else {
-						for (int i = 3; i < 6; i++) {
-							for (int j = 3; j < 6; j++) {
-								if (feld[j][i] == number) {
-									return false;
-								}
-							}
-						}
-					}
-				} else {
-					for (int i = 3; i < 6; i++) {
-						for (int j = 6; j < 9; j++) {
-							if (feld[j][i] == number) {
-								return false;
-							}
-						}
-					}
-				}
-			}
-		} else {
-			if (x < 6) {
-				if (x < 3) {
-					for (int i = 6; i < 9; i++) {
-						for (int j = 0; j < 3; j++) {
-							if (feld[j][i] == number) {
-								return false;
-							}
-						}
-					}
-				} else {
-					for (int i = 6; i < 9; i++) {
-						for (int j = 3; j < 6; j++) {
-							if (feld[j][i] == number) {
-								return false;
-							}
-						}
-					}
-				}
-			} else {
-				for (int i = 6; i < 9; i++) {
-					for (int j = 6; j < 9; j++) {
-						if (feld[j][i] == number) {
-							return false;
-						}
-					}
-				}
-			}
-		}
-		return true;
-	}
-
-	public int[][] dreierfeld(int x, int y) {
+	public boolean minifeldNL(int x, int y) {
 		int newx = 0;
 		int newy = 0;
 		if (x >= 0 && x <= 2) {
@@ -221,110 +160,44 @@ public class Autogenerate {
 		}
 		for (int i = newy; i < newy + 2; i++) {
 			for (int j = newx; j < newx + 2; j++) {
-				if (feld[j][i] == feld[y][x]) {
-					if (i != y && j != x) {
-						for (int c = 0; c < 9; c++) {
-							if (falsch[c][0] != feld[x][y]) {
-								falsch[i][0] = feld[x][y];
-								falsch[i][1] = x;
-								falsch[i][2] = y;
-							}
-						}
-					}
+				if (feld[j][i] != 0) {
+					return true;
 				}
 			}
 		}
-		for (int i = 0; i <= y; i++) {
-			for (int j = newx; j < newx + 2; j++) {
-				if (feld[j][i] == feld[y][x]) {
-					if (i != y && j != x) {
-
-					}
-				}
-			}
-		}
-		return falsch;
-	}
-
-	public boolean reihenfuellen() {
-		if (y == 8 && x == 9) { // letzte zelle befuellt
-			return true;
-		}
-
-		if (x == 9) {
-			y++;
-			x = 0;
-		}
-
-		Random random = new Random();
-
-		List<Integer> l = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-		if (x == 0) {
-			Collections.shuffle(l, random);
-		}
-
-		if (dreierfeld(x, y, l.get(x))) {
-			if (spalte(x, y, l.get(x))) {
-				if (reihe(x, y, l.get(x))) {
-					feld[x][y] = l.get(x);
-					x++;
-				} else {
-					Collections.shuffle(l, random);
-				}
-			} else
-
-			{
-				// Backtracking
-			}
-		} else {
-			// Backtracking
-			Collections.shuffle(l, random);
-		}
-		for (int i = 0; i < 9; i++) {
-			if (dreierfeld(i, y) == null) {
-
-			}
-		}
-		reihenfuellen();
 		return false;
 	}
 
 	public void fuellen(int wieViele) {
-		Random random = new Random();
-		int x = 0;
-		int y = 0;
-		int wert = 0;
-		while (soll != wieViele) {
-			x = random.nextInt(8) + 1;
-			y = random.nextInt(8) + 1;
-			while (feld[x][y] != 0) {
-				x = random.nextInt(8) + 1;
-				y = random.nextInt(8) + 1;
+		fertigesfeld = feld;
+		backtracking();
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				feld[i][j] = 0;
 			}
-			wert = random.nextInt(9) + 1;
-			if (reihe(x, y, wert) && spalte(x, y, wert)) {
-				if (dreierfeld(x, y, wert)) {
-					feld[x][y] = wert;
-					soll++;
-					System.out.println(soll + ".: feld[" + x + "][" + y + "]" + wert);
-				}
-				if (ist >= 81) {
-					print();
-				}
-			}
-			ist++;
 		}
+//		Random random = new Random();
+//		int howmany1 = wieViele / 9;
+//		int x = 0;
+//		int y = 0;
+//		while (soll != wieViele) {
+//			x = random.nextInt(2);
+//			y = random.nextInt(2);
+//			if (feld[x][y] == 0) {
+//				while (feld[x][y] == 0) {
+//					x = random.nextInt(2);
+//					y = random.nextInt(2);
+//				}
+//			} else {
+//				if (minifeldNL(x, y)) {
+//					feld[x][y] = 0;
+//					soll++;
+//				}
+//			}
+//		}
 	}
 
 	public void print() {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (feld[j][i] == 0) {
-					System.out.println("X: " + j + ", Y: " + i);
-				}
-			}
-		}
 		System.out.println(" ╔═══╦═══╦═══╦╦═══╦═══╦═══╦╦═══╦═══╦═══╗");
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
@@ -353,17 +226,9 @@ public class Autogenerate {
 	}
 
 	public static void main(String[] args) {
-		// Autogenerate ag = new Autogenerate(54);
-		// ag.print();
-		// System.out.println();
-		// System.out.println();
-		// System.out.println();
-		//
-		// Autogenerate agwithrows = new Autogenerate();
-		// agwithrows.print();
-		Random random = new Random();
-		while (true) {
-			System.out.println(random.nextInt(9));
-		}
+		Autogenerate ag = new Autogenerate();
+
+		ag.fuellen(10);
+		ag.print();
 	}
 }
