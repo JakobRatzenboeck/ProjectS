a4b4758764fdbb7d87d42488173c4f8e4dc084e5
Johannes <Johannes@10.91.75.170>
2017-04-07 10:24:06 +0200

    asdf

---
+++ src/Gui/Game.java
@@ -0,0 +1,61 @@
+package Gui;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+
+public class Game extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("SUDOKU");
+			
+			 Object[] sudoku = new Object[9][9];
+			int x = 0;
+			int y = 0;
+			int z = 0;
+			
+			
+			for(int i = 0;i < 81; i++)
+			{
+				Button a = new Button("" + z);
+				
+				
+			}
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+		
+
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	
+	
+}
